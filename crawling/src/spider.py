d3eed92756865475c1c56db9484dde109165a360
snitsaryuk <snitsaryuk@yandex-team.ru>
2024-03-04 11:03:21 +0300

    fix first task

--- crawling/src/spider.py
+++ crawling/src/spider.py
@@ -9,13 +9,18 @@ PAGES_COUNT = 100
 save_dir_ru = 'crawling/result/ru'
 index_file = 'crawling/result/index.txt'
 
-restricted_domains = ['t.me', 'instagram.com', 'vk.com', 'm.vk.com', 'ok.ru', 'youtube.com', 'www.youtube.com',
+restricted_domains = ['t.me', 'telegram.org', 'instagram.com', 'vk.com', 'm.vk.com', 'ok.ru', 'youtube.com', 'www.youtube.com',
                       'www.tiktok.com', 'viber.com', 'music.apple.com', 'rutube.ru', 'www.linkedin.com',
                       'linkedin.com', 'apps.apple.com', 'www.apple.com', 'github.com', 'account.ncbi.nlm.nih.gov',
-                      'kudago.com', 'www.zoom.com']
-restricted_urls = [
-    'https://zen.yandex.ru/tolkosprosit',
-]
+                      'kudago.com', 'www.zoom.com', 'swiftwithmajid.com', 'danielsaidi.com', 'www.kodeco.com',
+                      'help.kodeco.com', 'holyswift.app', 'account.habr.com', 'support.google.com', 'www.avanderlee.com',
+                      'developer.apple.com', 'fakestoreapi.com', 'en.wikipedia.org', 'devstreaming-cdn.apple.com',
+                      'apptractor.ru', 'connect.ok.ru', 'nodejs.org', 'page.hyoo.ru', 'idea.hyoo.ru', 'developer.mozilla.org',
+                      'ftdichip.com', 'www.donnywals.com', 'telegram.me', 'sso.passport.yandex.ru', 'passport.yandex.ru',
+                      'tenor.com', 'transparencyreport.google.com', 'techcrunch.com', 'de.wikipedia.org', 'no.wikipedia.org',
+                      'fa.wikipedia.org', 'it.wikipedia.org', 'oc.wikipedia']
+restricted_urls = ['https://zen.yandex.ru/tolkosprosit']
+restricted_paths = ['about', 'register', 'login', 'agreement', 'docs']
 start_urls = [
     'https://habr.com/ru/articles/797735/',
     'https://habr.com/ru/companies/productivity_inside/articles/797733/',
@@ -43,7 +48,8 @@ class PagesSpider(scrapy.Spider):
     def page_allowed(self, next_page):
         try:
             url_domain = next_page.split('/')[2]
-            if next_page in restricted_urls or url_domain in restricted_domains:
+            url_path = next_page.split('/')[3]
+            if next_page in restricted_urls or url_domain in restricted_domains or url_path in restricted_paths:
                 return False
             else:
                 return True
@@ -52,9 +58,15 @@ class PagesSpider(scrapy.Spider):
 
     def parse(self, response): 
         url_domain = response.url.split('/')[2]
-        if response.url in restricted_urls or url_domain in restricted_domains:
+        url_path = response.url.split('/')[3]
+        if response.url in restricted_urls or url_domain in restricted_domains or url_path in restricted_paths:
             self.log(f'{response.url} blocked by list during parsing')
         else:
+            lang_attribute = response.xpath('//html/@lang').get()
+            language = lang_attribute.strip()
+
+            if not language.startswith('ru'): return
+            
             filename = f'{self.page_counter}-{response.url.split("/")[-2]}.html'
             filepath = os.path.join(save_dir_ru, filename)
 
